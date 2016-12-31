63cc4886f0b355660534c8274834520fc7d2ba84
kenshin6 <jeffreycgoldman@gmail.com>
2016-12-31 13:15:09 +0900

    Layouts and routes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def full_title(page_title = '')
+    base_title = "A blog about coding, climbing, and Japan."
+    if page_title.empty?
+      base_title
+    else
+      page_title
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'home', to: 'static_pages#home', as: 'home'
-  get 'help', to: 'static_pages#help', as: 'help'
-  get 'about', to: 'static_pages#about', as: 'about'
   root 'static_pages#home'
+  get 'contact', to: 'static_pages#contact', as: 'contact'
+  get 'about', to: 'static_pages#about', as: 'about'
+  get 'signup', to: 'users#new', as: 'signup'
 end
