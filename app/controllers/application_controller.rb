752d509485157230594449ad42da9e53a32187a4
kenshin6 <jeffreycgoldman@gmail.com>
2017-01-09 12:24:23 +0900

    Add basic login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
-  def top_page
-    render html: "This is the top page!"
-  end
+  include SessionsHelper
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   root 'static_pages#home'
-  get 'contact', to: 'static_pages#contact', as: 'contact'
-  get 'about', to: 'static_pages#about', as: 'about'
-  get 'signup', to: 'users#new', as: 'signup'
-  post 'signup', to: 'users#create'
+  get 'contact',   to: 'static_pages#contact', as: 'contact'
+  get 'about',     to: 'static_pages#about',   as: 'about'
+  get 'signup',    to: 'users#new',            as: 'signup'
+  post 'signup',   to: 'users#create'
+  get 'login',     to: 'sessions#new'
+  post 'login',    to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
   resources :users
 end
