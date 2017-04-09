d3cfcf074635dc9211e554857a640c76a04ccd27
kenshin6 <jeffreycgoldman@gmail.com>
2017-04-09 22:13:41 +0900

    Add preview page and update views

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,20 +1,25 @@
 class PostsController < ApplicationController
-  before_action :get_post, except: [:create, :destroy]
+  before_action :get_post, except: [:create, :destroy, :new]
   before_action :logged_in_user, only: [:create, :destroy]
   before_action :admin_user,     only: [:create, :destroy]
   before_action :correct_user,   only: :destroy
 
-  def show
+  def new
+    @post = current_user.posts.build
   end
 
   def create
     @post = current_user.posts.build(post_params)
-    if @post.save
-      flash[:success] = "Post created!"
-      redirect_to root_url
+    if params[:preview_button]
+      render 'preview'
     else
-      @feed_items = []
-      render 'static_pages/home'
+      if @post.save
+        flash[:success] = "Post created!"
+        redirect_to @post
+      else
+        @feed_items = []
+        render 'static_pages/home'
+      end
     end
   end
 
@@ -26,10 +31,16 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
     if @post.update_attributes(post_params)
       flash[:success] = "Post updated"
-      redirect_to root_path
+      redirect_to @post
     else
       render 'edit'
     end
+  end
+
+  def show
+  end
+
+  def preview
   end
 
   def destroy
--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,9 @@ Rails.application.routes.draw do
   resources :users
   resources :account_activations, only: [:edit]
   resources :password_resets,     only: [:new, :create, :edit, :update]
-  resources :posts
+  resources :posts do
+    member do
+      get 'preview'
+    end
+  end
 end
